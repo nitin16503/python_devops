8106a7fef61b0583379399f4160ba1add2ef8265
nitinn16503 <nitinmehta16may@gmail.com>
2024-01-08 10:47:22 +0530

    Functions, Modules Packages & Virtual env.

--- 4Day.py
+++ 4Day.py
@@ -1,19 +1,50 @@
-#                                                                     𝔽𝕦𝕟𝕔𝕥𝕚𝕠𝕟𝕤, 𝕄𝕠𝕕𝕦𝕝𝕖𝕤 𝕒𝕟𝕕 ℙ𝕒𝕔𝕜𝕒𝕘𝕖𝕤🙌
+#                                                                      🙌 𝔽𝕦𝕟𝕔𝕥𝕚𝕠𝕟𝕤 🙌
+#                          Calculator 
+# a = 1  
+# b = 2 
 
-#   calculator 
-a = 1  
-b = 2 
+# def add (): 
+#     print(a+b)
+# def Sub() :
+#     print(a-b)
+# def mul():
+#     print(a*b)
+# def div(x,z):
+#     print (x/z)
 
-def add (): 
-    print(a+b)
-def Sub() :
-    print(a-b)
-def mul():
-    print(a*b)
-def div(x,z):
-    print (x/z)
+# add() 
+# Sub()
+# mul()
+# div(100,2)
 
-add() 
-Sub()
-mul()
-div(100,2)
+#                                                                      🙌 𝕄𝕠𝕕𝕦𝕝𝕖𝕤 🙌
+import z_my_module as hemlo
+
+result = hemlo.multiplication (10000,10)
+print (result)
+result1 = hemlo.addition (10000,10)
+print (result1)
+result2 = hemlo.Subtraction (10000,10)
+print (result2)
+result3 = hemlo.division (10000,10)
+print (result3)
+
+#                                                                    🙌 ℙ𝕒𝕔𝕜𝕒𝕘𝕖𝕤 🙌
+# A package is a collection of modules organized by python developers/contributers.
+
+# Where to to find packages? 🔍PYPI 
+
+# HOW TO i  INSTALL PACKAGES❓
+# ➡️pip install package_name (⚠️inside terminal)
+#  pip install boto3
+
+#                                                               🙌 𝕍𝕚𝕣𝕥𝕦𝕒𝕝 𝔼𝕟𝕧𝕚𝕣𝕠𝕟𝕞𝕖𝕟𝕥 🙌
+# In devops we use common machine and different projects require diffrent verions of packages. 
+# For solving this problem we require virtual environment. For each project you create virtual environment which act as a seperate machine.
+
+#  HOW TO DO?
+#  1st -> pip install virtualenv
+#  2nd -> python -m venv PROJET_NAME     (create seperated machine(virtual environment) with project PROJECT_NAME )
+#  3rd -> source PROJECT_NAME/bin/activate  (enter the virtual environment)
+#  4th -> deactivate     (exit virtul environment)
+
