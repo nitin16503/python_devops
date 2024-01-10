be3714f65f22bab98b280b10c9218763f2c5f575
nitinn16503 <nitinmehta16may@gmail.com>
2024-01-10 10:27:27 +0530

    operators

---
+++ 6Day.py
@@ -0,0 +1,35 @@
+#                                                             🙌🙌 𝕆𝕡𝕖𝕣𝕒𝕥𝕠𝕣𝕤
+
+# Arithmetic Operators: These operators are used for performing basic mathematical operations such as addition, subtraction, multiplication, division, and more.
+print (3/2) # 1.5 
+print (3//2)# 1
+print (5%3) # 2
+
+# Assignment Operators: Assignment operators are used to assign values to variables. They include the equal sign (=) and various compound assignment operators.
+a=1
+a+=2
+a-=2
+
+# Relational Operators: Relational operators are used to compare values and determine the relationship between them. They return a Boolean result (True or False).
+x=2
+y=3
+print(x>y) #false
+print(x>=y) #false
+print(x<y)  #true
+print(x<=y) #true
+print(x==2) #true 
+
+
+# Logical Operators: Logical operators are used to combine and manipulate Boolean values. They include "and," "or," and "not."
+t = True
+f = False
+print (t and f ) #false
+print (t or f)   # true
+print (not f)    # true 
+
+# Identity Operators: Identity operators are used to check if two variables point to the same object in memory. The identity operators in Python are "is" and "is not.
+a=5
+b=5
+print (a is b ) #True
+print (a is not b)#False
+
--- z_my_module_day4.py
+++
@@ -1,9 +0,0 @@
-
-def addition (a,b): 
-    return (a+b)
-def Subtraction(a,b) :
-    return (a-b)
-def multiplication(a,b):
-    return (a*b)
-def division(x,z):
-    return (x/z)
